1bac8680e1b1f95a685c5479322066bc2ffc26cf
StaVergos <stavrospvergos@gmail.com>
2024-12-24 12:12:15 +0200

    Optimistic concurrency control on updates

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -129,10 +129,14 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	err = app.models.Movies.Update(movie)
 	if err != nil {
-		app.serverErrorResponse(w, r, err)
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
 		return
 	}
-	// Write the updated movie record in a JSON response.
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
