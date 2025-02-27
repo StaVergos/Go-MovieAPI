e4e4828d0f8321bc3ee6930ee42ab9baa4eebbb7
StaVergos <stavrospvergos@gmail.com>
2025-02-27 18:04:35 +0200

    Added filters for a get all movies route

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,7 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"movieapi/internal/validator"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
@@ -85,4 +87,37 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
+}
+
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+	return s
+}
+
+func (app *application) readCSV(qs url.Values, key string, defaultValues []string) []string {
+	csv := qs.Get(key)
+
+	if csv == "" {
+		return defaultValues
+	}
+	return strings.Split(csv, ",")
+}
+
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+	return i
 }
