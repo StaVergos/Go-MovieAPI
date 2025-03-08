f0bedd290908fcca466c59c59f7ac793ccaec8ea
StaVergos <stavrospvergos@gmail.com>
2025-03-08 18:57:03 +0200

    Add sorting lists

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"movieapi/internal/validator"
 	"time"
 
@@ -147,7 +148,7 @@ func (m MovieModel) Delete(id int64) error {
 }
 
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `
+	query := fmt.Sprintf(`
 		SELECT
 			id,
 			created_at,
@@ -157,9 +158,9 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 			genres,
 			version
 		FROM movies
-		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
-		AND (genres @> $2 OR $2 = '{}') 
-		ORDER BY id`
+        WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
+        AND (genres @> $2 OR $2 = '{}')     
+        ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
