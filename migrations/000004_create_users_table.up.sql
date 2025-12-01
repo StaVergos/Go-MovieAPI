68fdf23b8203ba53a3e1bd607fd40e8e1e81e6e9
StaVergos <stavrospvergos@gmail.com>
2025-12-01 14:04:33 +0200

    Fixed reversed name of migration files

--- migrations/000004_create_users_table.up.sql
+++ migrations/000004_create_users_table.up.sql
@@ -1,1 +1,10 @@
-DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    name text NOT NULL,
+    email citext UNIQUE NOT NULL,
+    password_hash bytea NOT NULL,
+    activated bool NOT NULL,
+    version integer NOT NULL DEFAULT 1
+);
