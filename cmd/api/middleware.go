a38b429ed5b25efd0e69ea31b7fe96c421454947
StaVergos <stavrospvergos@gmail.com>
2026-02-02 12:00:06 +0200

    Added permission middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -147,3 +147,24 @@ func (app *application) requiredAuthenticatedUser(next http.HandlerFunc) http.Ha
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requirePermission(code string, next http.HandlerFunc) http.HandlerFunc {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		permissions, err := app.models.Permissions.GetAllForUser(user.ID)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		if !permissions.Include(code) {
+			app.notPermittedResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+
+	return app.requiredAuthenticatedUser(fn)
+}
